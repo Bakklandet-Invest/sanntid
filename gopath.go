299431e6c51dfe253a55d903ef81f5a96967a1a3
fredosavage <fredrikskr@gmail.com>
2015-02-17 17:42:48 +0100

    forelesningsnotater

---
+++ gopath.go
@@ -0,0 +1,79 @@
+gopath
+
+[10]byte // array of 10 bytes
+
+Public / private variables
+stor forbokstav er public
+liten - private
+
+slice vs array
+slices play the role of dynamically sized arrays
+slices are part of an underlying array.
+changing the underlaying array will change the slice covering that part
+
+
+map iteration order is not specified
+
+for key, value := range m {
+	value = value + 1 //wont work (assigns to a temporary variable)
+	m[key] = value + 1 // assign to the object inside the map
+}
+
+func Printf(format string, args ...int){ // ubestemt mengde input arg. blir lagret i array "args"
+	if len(args)>0{
+
+	}
+}
+
+type Point strukct {
+		x, y int
+}
+
+func main(){
+	p := point{2,3}
+	fmt.Println(p.String())
+	p.x = 4
+	fmt.println(Point{3,5}.string())
+}
+
+----------------------------
+
+go doc - kan vise documentation for funksjoner i en package
+godoc.org
+STOR forbokstav i en funksjon vil gjøre den public i en package
+liten forbokstav vil gjøre den private
+
+-----------------------------
+
+const (
+	Mon Weekday = iota // iota gir Mon verdien 0
+	Tue   //alle under Mon blir av typen Weekday
+	Wed
+	Thu
+
+	)
+
+-------------------------------
+
+INTERFACE
+
+interface{} // empty interface
+
+interface {
+	String( string)
+}
+
+interface{
+	Len() int
+	Swap(i, j int)
+	Less(i, j, int) bool
+}
+
+type tor struct{
+	name string
+}
+
+func (t tor) Len() int{
+	len(t.name)
+}
+
