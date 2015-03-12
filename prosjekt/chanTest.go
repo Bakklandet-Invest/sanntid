f56a70b67881d8eac75a4bba25e97919544babbe
fredosavage <fredrikskr@gmail.com>
2015-03-12 12:54:43 +0100

    tester channels

--- prosjekt/chanTest.go
+++ prosjekt/chanTest.go
@@ -35,11 +35,13 @@ func main(){
 	//text, _ := chanReader.ReadString()
 	fmt.Println(text)
 
-	host, _ := os.Hostname()
-	addrs, _ := net.LookupIP(host)
-	for _, addr := range addrs {
-    	if ipv4 := addr.To4(); ipv4 != nil {
-        	fmt.Println("IPv4: ", ipv4)
-    }   
-}
+	addrs, err := net.InterfaceAddrs()
+	for _, address := range addrs {
+        // check the address type and if it is not a loopback the display it
+        if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+            if ipnet.IP.To4() != nil {
+                fmt.Println(ipnet.IP.String())
+            }
+        }
+    }
 }
