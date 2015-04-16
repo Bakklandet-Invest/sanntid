800bd33b45164ad265dd80d6e57b4f00fbde23bf
fredosavage <fredrikskr@gmail.com>
2015-04-16 12:28:40 +0200

    fikset gopaths og startet på nettverk

---
+++ prosjekt/src/control/control.go
@@ -0,0 +1,23 @@
+package control
+
+import (
+	"network"
+	
+	)
+
+type Control struct {
+	elevators map[string]*network.Elevator
+	orders map[int][2]bool
+	connected map[string]bool
+}
+
+func (cont *Control) InitMaps() {
+	
+	cont.elevators = make(map[string]*network.Elevator)
+	cont.connected = make(map[string]bool)
+	cont.orders = make(map[int][2]bool)
+	
+	return
+}
+
+
